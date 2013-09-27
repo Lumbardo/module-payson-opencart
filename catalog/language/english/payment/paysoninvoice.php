5bee58ae8cad8b9851524a49e8ee9715e5567504
osca-vil <osca-vil@PC566.svea_dom>
2013-09-27 11:17:30 +0200

    updated the text for  invoice terms.

--- catalog/language/english/payment/paysoninvoice.php
+++ catalog/language/english/payment/paysoninvoice.php
@@ -1,6 +1,6 @@
 <?php
 
-$_['text_title']   = '<img src="https://www.payson.se/sites/all/files/images/external/payson_faktura.png" style="width:140px;height:55px;clear:right" alt="Payson Invoice" title="Paysoninvoice" />  - (Invoice fee: %s SEK)';
+$_['text_title']   = '<img src="https://www.payson.se/sites/all/files/images/external/payson_faktura.png" style="width:140px;height:55px;clear:right" alt="Payson Invoice" title="Paysoninvoice" />  - (Invoice fee inc VAT: %s SEK)';
 $_['text_denied'] = 'The payment was denied.';
 $_['text_payson_payment_method'] = ' Please try using a different payment method.';
 ?>
