8990f661ef02598d16d4be3c5002ed34335b5858
Daniel Hansen <daniel.hansen@payson.se>
2013-10-21 13:49:48 +0200

    * Implemented a fix for the issue with roundings causing "The amount specified by the receiver"-errors.

--- catalog/controller/payment/paysondirect.php
+++ catalog/controller/payment/paysondirect.php
@@ -6,7 +6,7 @@ class ControllerPaymentPaysondirect extends Controller {
     private $api;
     private $isInvoice;
 
-    const MODULE_VERSION = '2.9.1';
+    const MODULE_VERSION = '2.9.2';
 
     function __construct($registry) {
         parent::__construct($registry);
@@ -293,7 +293,7 @@ class ControllerPaymentPaysondirect extends Controller {
 
         $order_data = $this->model_checkout_order->getOrder($this->session->data['order_id']);
 
-        $query = "SELECT `order_product_id`, `name`, `model`, `price`, `quantity`, `tax` / `price` AS 'tax_rate' FROM `" . DB_PREFIX . "order_product` WHERE `order_id` = " . (int) $orderId . " UNION ALL SELECT 0, '" . $this->language->get('text_gift_card') . "', `code`, `amount`, '1', 0.00 FROM `" . DB_PREFIX . "order_voucher` WHERE `order_id` = " . (int) $orderId;
+        $query = "SELECT `order_product_id`, `name`, `model`, `price`, `quantity`, `tax` FROM `" . DB_PREFIX . "order_product` WHERE `order_id` = " . (int) $orderId . " UNION ALL SELECT 0, '" . $this->language->get('text_gift_card') . "', `code`, `amount`, '1', 0.00 FROM `" . DB_PREFIX . "order_voucher` WHERE `order_id` = " . (int) $orderId;
         $product_query = $this->db->query($query)->rows;
 
         foreach ($product_query as $product) {
@@ -313,7 +313,7 @@ class ControllerPaymentPaysondirect extends Controller {
             
             $product_price = $this->currency->format($product['price'], $order_data['currency_code'], $order_data['currency_value'], false);
 
-            $this->data['order_items'][] = new OrderItem(html_entity_decode($productTitle, ENT_QUOTES, 'UTF-8'), $product_price, $product['quantity'], $product['tax_rate'], $product['model']);
+            $this->data['order_items'][] = new OrderItem(html_entity_decode($productTitle, ENT_QUOTES, 'UTF-8'), $product_price, $product['quantity'], Round($product['tax']/$product['price'], 3), $product['model']);
         }
 
         $orderTotals = $this->getOrderTotals();
