30c4f5a7998b5c5f2f43beb7fbcb02ae9db47b40
Lumbardo <olaussonjan@glocalnet.net>
2014-02-16 11:38:46 +0100

    Update paysoninvoice_fee.php

--- admin/language/swedish/total/paysoninvoice_fee.php
+++ admin/language/swedish/total/paysoninvoice_fee.php
@@ -8,7 +8,7 @@ $_['text_success']     = 'Klart: Inställningarna har ändrats!';
 
 // Entry
 $_['entry_total']      = 'Order Total:';
-$_['entry_fee']        = 'Avgift:<br /><span class="help">Retail-priset med moms m?ste vara i spannet 0 - 40SEK <br />Momsen m?ste vara 25%</span>';
+$_['entry_fee']        = 'Avgift:<br /><span class="help">Retail-priset med moms måste vara i spannet 0 - 40SEK <br />Momsen måste vara 25%</span>';
 $_['entry_tax_class']  = 'Momsklass:';
 $_['entry_status']     = 'Status:';
 $_['entry_sort_order'] = 'Sorteringsordning:';
